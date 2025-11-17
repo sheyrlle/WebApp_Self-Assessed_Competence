a0e614ce0892988c128e7e773c1c19a07a93b13d
sheyrlle <168162033+sheyrlle@users.noreply.github.com>
2025-11-18 02:55:53 +0800

    Update WebApp3.py

--- WebApp3.py
+++ WebApp3.py
@@ -10,10 +10,10 @@ import pickle
 VECTOR_PATH = "vectorizer.pkl"
 MODEL_PATH = "rf_model.pkl"
 
-VECTOR_FILE_ID = "1v2c994eZZr6vhHntz-sEtzN6OnyVcmcQ"
-# https://drive.google.com/file/d/1v2c994eZZr6vhHntz-sEtzN6OnyVcmcQ/view?usp=drive_link
-MODEL_FILE_ID = "17i_9AHz0WAyLq7W9VPBkyc2_vKbmmN98"
-# https://drive.google.com/file/d/17i_9AHz0WAyLq7W9VPBkyc2_vKbmmN98/view?usp=drive_link
+VECTOR_FILE_ID = "19aoX8xUERWUKarzReoFKH7bHB6z13gYR"
+# https://drive.google.com/file/d/19aoX8xUERWUKarzReoFKH7bHB6z13gYR/view?usp=drive_link
+MODEL_FILE_ID = "1tHndbQPlIQ255ZIYDym0DxH66MKJo_zy"
+# https://drive.google.com/file/d/1tHndbQPlIQ255ZIYDym0DxH66MKJo_zy/view?usp=drive_link
 
 # Convert to direct download URLs
 VECTOR_URL = f"https://drive.google.com/uc?id={VECTOR_FILE_ID}"
@@ -150,6 +150,7 @@ elif page == "History":
             st.info("No sentiment history found yet.")
     else:
         st.info("No sentiment history file found yet.")
+
 
 
 
